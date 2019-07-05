3e211aa9a745572ab79aa48f4927eb2bffc25267
rtrad <rafi.trad@st.ovgu.de>
2019-07-05 18:50:50 +0200

    Loading LSS BoT into row-wise dataframe

--- pocs/lss_modeller.py
+++ pocs/lss_modeller.py
@@ -4,9 +4,12 @@
 """
 import subprocess as s
 import os
+from shutil import rmtree
 from gensim.corpora import Dictionary, bleicorpus
 # from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import time
+import pandas as pd
 
 
 class LssModeller:
@@ -14,7 +17,7 @@ class LssModeller:
 
     # Constructor
     def __init__(self, hdp_path: str, input_docs_path: str,
-                 ldac_filename: str):
+                 ldac_filename: str, hdp_output_dir: str):
         """Default Constructor
 
         Parameters
@@ -30,74 +33,112 @@ class LssModeller:
         self.hdp_path = hdp_path
         self.input_docs_path = input_docs_path
         self.lda_c_fname = ldac_filename
+        self.hdp_output_directory = hdp_output_dir
 
-    def _read_corpus_into_docline(self) -> list:
+    def _initialise_directory(self, dir_path):
+        try:
+            if os.path.exists(dir_path):
+                rmtree(dir_path)
+            os.mkdir(dir_path)
+        except PermissionError:
+            print("> ERROR: Please make sure the {} folder"
+                  "is not used by some process").format(dir_path)
+
+    def _convert_corpus_to_bow(self) -> list:
+        """Convert a directory of text files into a BoW model"""
+        # Read in the plain text files
         plain_documents = []
         with os.scandir(self.input_docs_path) as docs:
             for doc in docs:
-                f = open(doc.path, "r")
-                plain_documents.append(f.read())
-        return plain_documents
+                try:
+                    f = open(doc.path, "r")
+                    plain_documents.append(f.read())
+                except PermissionError:
+                    # Raised when trying to open a directory
+                    print("\tDirectory {} skipped".format(doc.path))
+                    pass
 
-    def _tokenise_clean_corpus(self, docline: list) -> list:
-        cleaned_corpus = [
+        # Tokenise corpus and remove empty documents
+        tokenised_corpus = [
                 [w for w in word_tokenize(d.lower())]
-                for d in docline]
-        return cleaned_corpus
+                for d in plain_documents if len(d) > 0]
 
-    def _convert_tokenised_docs_to_bow(self, docline: list) -> list:
-        tokenised_corpus = self._tokenise_clean_corpus(docline=docline)
         # Form the word ids dictionary for vectorisation
         dictionary = Dictionary(tokenised_corpus)
         bow_corpus = [dictionary.doc2bow(t_d) for t_d in tokenised_corpus]
-        return bow_corpus
+        return(bow_corpus, dictionary)
 
-    def _store_docline_as_lda_c(self, docline: list):
-        """ Convert a docline corpus to BoW and then to LDA_C and store it"""
+    def _generate_lda_c_corpus(self):
+        """ Convert a group of files LDA_C corpus and store it on disk"""
         print("> Converting doclines into LDA-C and storing to disk")
-        bow_corpus = self._convert_tokenised_docs_to_bow(docline=docline)
+        bow_corpus, id2word_map = self._convert_corpus_to_bow()
         # Sterialise into LDA_C and store on disk
         output_dir = r"{}\lda_c_format".format(self.input_docs_path)
-        if os.path.exists(output_dir):
-            os.shutil.rmtree(output_dir)
-        os.mkdir(output_dir)
+        self._initialise_directory(output_dir)
 
         save_location = r"{}\{}.dat".format(
                 output_dir, self.lda_c_fname)
         bleicorpus.BleiCorpus.serialize(
-                fname=save_location, corpus=bow_corpus)
+                fname=save_location, corpus=bow_corpus,
+                id2word=id2word_map)
 
-    def reform_corpus_as_LDA_C(self):
-        """Read the plain documents and save them as LDA-C for hdp"""
-        print("> Reforming corpus into LDA-C and savinf it as {}.dat".format(
-                self.lda_c_fname))
-        docline = self._read_corpus_into_docline()
-        self._store_docline_as_lda_c(docline=docline)
-
-    def _invoke_gibbs_hdp(self):
+    def _invoke_gibbs_hdp(self, param_num_iter):
         """Invoke Gibbs hdp posterior inference on the corpus"""
-        ret = s.run(
-                [r"{}\hdp.exe".format(
-                        self.master_docs_path),
-                    "--algorithm",
-                    "train",
-                    "--data",
-                    r"{}\data\fullBleiCorpus.dat".format(
-                            self.master_docs_path),
-                    "--directory",
-                    r"{}\results_hdp".format(
-                            self.master_docs_path),
-                    "--save_lag",
-                    "-1"],
-                check=True, capture_output=True, text=True)
+        path_executable = r"{}\hdp.exe".format(self.hdp_path)
+        param_data = r"{}\lda_c_format\{}.dat".format(self.input_docs_path,
+                                                      self.lda_c_fname)
+        param_directory = r"{}\{}".format(self.input_docs_path,
+                                          self.hdp_output_directory)
+        # Prepare the output directory
+        self._initialise_directory(param_directory)
+
+        ret = s.run([path_executable,
+                     "--algorithm",     "train",
+                     "--data",          param_data,
+                     "--directory",     param_directory,
+                     "--max_iter",      param_num_iter,
+                     "--save_lag",      "-1"],
+                    check=True, capture_output=True, text=True)
 
         return ret.stdout
+
+    def infer_lss_representation(self, param_num_iter="25"):
+        """Produce an LSS representation of text files and save it to disk"""
+        # Make the text files into an LDA-C corpus
+        self._generate_lda_c_corpus()
+        # Run Gibbs HDP on the LDA-C corpus
+        print("\n> Starting HDP with {} iterations...".format(param_num_iter))
+        t = time.perf_counter()
+        output_hdp = self._invoke_gibbs_hdp(param_num_iter=param_num_iter)
+        nt = time.perf_counter()
+        print("************************************************************\n")
+        print(("HDP executed in {x:0.2f} seconds "
+              "and yielded the message:\n{y}"
+               ).format(x=nt-t, y=output_hdp))
+        print("**************************************************************")
+
+    def load_lss_representation_into_df(self) -> pd.DataFrame:
+        """Load a BoT LSS representation from disk to a returned dataframe"""
+        path = r"{}\{}\mode-word-assignments.dat".format(
+                self.input_docs_path,
+                self.hdp_output_directory)
+        # We don't need document tables, so we'll skip the relative column,
+        # But we do need word counts under each topic, to produce some sort
+        # of a bag-of-topics model (BoT)
+        lss_df = pd.read_csv(filepath_or_buffer=path, delim_whitespace=True)
+#                             usecols=["d", "w", "z"]).drop_duplicates()
+        # Procude topic weights as counts of topic words
+        lss_df = pd.DataFrame(
+                lss_df.groupby(by=["d", "z"]).w.count()
+                ).reset_index()
+        return lss_df
 
 
 Modeller = LssModeller(hdp_path=r"..\..\hdps\hdp",
                        input_docs_path=r"..\data\toy_corpus",
-                       ldac_filename="dummy_ldac_corpus")
-Modeller.reform_corpus_as_LDA_C()
+                       ldac_filename=r"dummy_ldac_corpus",
+                       hdp_output_dir=r"hdp_lss")
+df = Modeller.load_lss_representation_into_df()
 # =============================================================================
 # def main():
 #     print("Main thread started..\n")
