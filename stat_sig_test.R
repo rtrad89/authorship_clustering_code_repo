bd4a66362b82fc0d05c5cfed2febf0064885a339
rtrad89 <rafi.trad@ovgu.de>
2020-10-05 09:51:41 +0200

    Run statistical tests on raw data; test for assumption internally.

--- stat_sig_test.R
+++ stat_sig_test.R
@@ -2,6 +2,7 @@
 library(readr)
 library(scmamp)
 
+
 setwd(file.path("D:", "Projects", "Authorial_Clustering_Short_Texts_nPTM",
                 "Code", "authorship_clustering_code_repo"))
 
@@ -10,12 +11,12 @@ posthoc_test_plot = function(results_fpath, use_minimal = T, my.alpha = 0.05)
   methods_performance <- read_csv(results_fpath)
   if(use_minimal)
   {
-    names(methods_performance)[7] = "COP_KMeans"
-    names(methods_performance)[11] = "SPKMeans"
+    names(methods_performance)[6] = "COP_KMeans"
+    names(methods_performance)[4] = "SPKMeans"
   }
   # Create a proper matrix
   mtx_perf = as.matrix(methods_performance[,-1])
-  row.names(mtx_perf) = methods_performance[, 1]$Dataset
+  row.names(mtx_perf) = methods_performance[, 1]$set
 
   if(use_minimal)
   {
@@ -23,6 +24,23 @@ posthoc_test_plot = function(results_fpath, use_minimal = T, my.alpha = 0.05)
     mtx_perf = mtx_perf[, colsnames]
   }
 
+  # Test for normality
+  normality_violated = FALSE
+  for(method in c("BL_r","BL_s","BL_SOTA_le", "SPKMeans", "COP_KMeans"))
+  {
+    if(length(unique(mtx_perf[, method])) <= 3)
+      next
+
+    p = shapiro.test(unname(mtx_perf[, method]))$p.value
+    if (p <= my.alpha)
+    {
+      normality_violated = TRUE
+      break
+    }
+  }
+
+  if(normality_violated)
+    print("Normality was violated")
 
   # Start the tests
   if (scmamp::friedmanAlignedRanksTest(mtx_perf)$p.value < my.alpha) {
@@ -41,7 +59,7 @@ posthoc_test_plot = function(results_fpath, use_minimal = T, my.alpha = 0.05)
 # Combine plots on one row
 par(mfrow = c(1,2))
 #B3F
-posthoc_test_plot("__outputs__/TESTS/methods_b3f_performance.csv")
+posthoc_test_plot("__outputs__/TESTS/raw_methods_b3f_performance.csv")
 #ARI
-posthoc_test_plot("__outputs__/TESTS/methods_ari_performance.csv")
+posthoc_test_plot("__outputs__/TESTS/raw_methods_ari_performance.csv")
 
